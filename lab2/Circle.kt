20c343fc97761ca8e7d7bdf2f15e81c35696dd52
Bumblbeer <10899714+Bumblbeer@users.noreply.github.com>
2020-12-21 15:09:13 +0300

    Changed structure of this folder

---
+++ lab2/Circle.kt
@@ -0,0 +1,33 @@
+import java.lang.IllegalArgumentException
+import kotlin.math.*
+
+class Circle(val radius: Float) : Shape {
+    init {
+        if (radius <= 0)
+            throw IllegalArgumentException("Radius can't be a non-natural value")
+    }
+
+    override fun getArea(): Float {
+        return PI.toFloat() * radius.pow(2.0F)
+    }
+
+    override fun getPerimeter(): Float {
+        return 2 * PI.toFloat() * radius
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Circle
+
+        if (radius != other.radius) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return radius.hashCode()
+    }
+
+}
---
+++ lab2/Shape.kt
@@ -0,0 +1,4 @@
+interface Shape {
+    fun getArea(): Float
+    fun getPerimeter(): Float
+}
