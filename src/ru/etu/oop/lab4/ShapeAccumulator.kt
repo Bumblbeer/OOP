b1b856110b9310bb0b464d34d5ca78375d86b30f
Bumblbeer <10899714+Bumblbeer@users.noreply.github.com>
2020-12-21 17:29:15 +0300

    added lab4

--- src/ru/etu/oop/lab4/ShapeAccumulator.kt
+++ src/ru/etu/oop/lab4/ShapeAccumulator.kt
@@ -3,4 +3,36 @@ import ru.etu.oop.lab2.Shape
 
 class ShapeAccumulator {
     private val shapesList = mutableListOf<Shape>()
+
+    fun add(shape: Shape) {
+        shapesList.add(shape)
+    }
+
+    fun addAll(shapesCol : Collection<Shape>) {
+        shapesList.addAll(shapesCol)
+    }
+
+    fun getMaxAreaShape(): Shape? {
+        return shapesList.maxByOrNull { shape -> shape.getArea() }
+    }
+
+    fun getMinAreaShape() : Shape? {
+        return shapesList.minByOrNull { shape -> shape.getArea() }
+    }
+
+    fun getMaxPerimeterShape() : Shape? {
+        return shapesList.maxByOrNull { shape -> shape.getPerimeter() }
+    }
+
+    fun getMinPerimeterShape() : Shape? {
+        return shapesList.minByOrNull { shape -> shape.getPerimeter() }
+    }
+
+    fun getTotalArea() : Float {
+        return shapesList.map { shape -> shape.getArea() }.sum()
+    }
+
+    fun getTotalPerimeter() : Float {
+        return shapesList.map { shape -> shape.getPerimeter() }.sum()
+    }
 }
